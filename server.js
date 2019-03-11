3954cd5f1869b87bcd05040631f0368a3c80610d
Bryant <cabrera.bryant@gmail.com>
2019-03-11 11:14:11 -0700

    Sets up server.js and adds middleware.

---
+++ server.js
@@ -0,0 +1,26 @@
+/********** REQUIRES **********/
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const session = require('express-session');
+
+require('./db/db');
+
+/********** MIDDLEWARE **********/
+app.use(session({
+    secret: "THIS IS A RANDOM STRING SECRET",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+
+
+/********** LISTENER **********/
+app.listen(process.env.PORT, () => {
+    console.log('Server is listening on port 3000.');
+});
