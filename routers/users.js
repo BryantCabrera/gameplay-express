4a118f0c1f3080d207106ba2769e5f7bebc801e3
Bryant <cabrera.bryant@gmail.com>
2019-03-11 11:42:47 -0700

    Creates Create Route for users in users.js.

--- routers/users.js
+++ routers/users.js
@@ -1,6 +1,7 @@
 /********** REQUIRES **********/
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
 const Game = require('../models/game');
@@ -13,9 +14,29 @@ router.get('/', async (req, res) => {
     try {
         const allUsers = await User.find({});
 
-        return allUsers
+        res.json({
+            status: 200,
+            data: 'Get all users successful.',
+            users: allUsers
+        })
     } catch (err) {
         res.send(err);
     }
 });
 
+//Create Route
+router.post('/', async (req, res) => {
+    let hashedPassword = crypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    req.body.password = hashedPassword;
+    try {
+        const createdUser = await User.create(req.body);
+        res.json({
+            status: 200,
+            data: 'login successful',
+            createdUser: createdUser
+        });
+        
+    } catch (err) {
+        res.send(err);
+    }
+});
