570aecf38a0c1cce0fc0c28276447871bb735af3
Bryant <cabrera.bryant@gmail.com>
2019-03-11 11:35:35 -0700

    Sets up users.js router.

---
+++ routers/users.js
@@ -0,0 +1,10 @@
+/********** REQUIRES **********/
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user');
+const Game = require('../models/game');
+
+/********** MIDDLEWARE **********/
+
+/********** ROUTES **********/
